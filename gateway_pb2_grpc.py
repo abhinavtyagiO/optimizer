30fbe1c119013c2af5c872cdfb7135d1514c5352
abhinavtyagiO <abhinav_t@pp.iitr.ac.in>
2026-04-01 14:19:50 +0530

    Update proto file

--- gateway_pb2_grpc.py
+++ gateway_pb2_grpc.py
@@ -5,26 +5,23 @@ import warnings
 
 import gateway_pb2 as gateway__pb2
 
-GRPC_GENERATED_VERSION = "1.80.0"
+GRPC_GENERATED_VERSION = '1.80.0'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
 try:
     from grpc._utilities import first_version_is_lower
-
-    _version_not_supported = first_version_is_lower(
-        GRPC_VERSION, GRPC_GENERATED_VERSION
-    )
+    _version_not_supported = first_version_is_lower(GRPC_VERSION, GRPC_GENERATED_VERSION)
 except ImportError:
     _version_not_supported = True
 
 if _version_not_supported:
     raise RuntimeError(
-        f"The grpc package installed is at version {GRPC_VERSION},"
-        + " but the generated code in gateway_pb2_grpc.py depends on"
-        + f" grpcio>={GRPC_GENERATED_VERSION}."
-        + f" Please upgrade your grpc module to grpcio>={GRPC_GENERATED_VERSION}"
-        + f" or downgrade your generated code using grpcio-tools<={GRPC_VERSION}."
+        f'The grpc package installed is at version {GRPC_VERSION},'
+        + ' but the generated code in gateway_pb2_grpc.py depends on'
+        + f' grpcio>={GRPC_GENERATED_VERSION}.'
+        + f' Please upgrade your grpc module to grpcio>={GRPC_GENERATED_VERSION}'
+        + f' or downgrade your generated code using grpcio-tools<={GRPC_VERSION}.'
     )
 
 
@@ -38,11 +35,10 @@ class OptimizerStub(object):
             channel: A grpc.Channel.
         """
         self.OptimizePrompt = channel.unary_unary(
-            "/gateway.Optimizer/OptimizePrompt",
-            request_serializer=gateway__pb2.OptimizationRequest.SerializeToString,
-            response_deserializer=gateway__pb2.OptimizationResponse.FromString,
-            _registered_method=True,
-        )
+                '/gateway.Optimizer/OptimizePrompt',
+                request_serializer=gateway__pb2.OptimizationRequest.SerializeToString,
+                response_deserializer=gateway__pb2.OptimizationResponse.FromString,
+                _registered_method=True)
 
 
 class OptimizerServicer(object):
@@ -51,46 +47,43 @@ class OptimizerServicer(object):
     def OptimizePrompt(self, request, context):
         """Missing associated documentation comment in .proto file."""
         context.set_code(grpc.StatusCode.UNIMPLEMENTED)
-        context.set_details("Method not implemented!")
-        raise NotImplementedError("Method not implemented!")
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
 
 
 def add_OptimizerServicer_to_server(servicer, server):
     rpc_method_handlers = {
-        "OptimizePrompt": grpc.unary_unary_rpc_method_handler(
-            servicer.OptimizePrompt,
-            request_deserializer=gateway__pb2.OptimizationRequest.FromString,
-            response_serializer=gateway__pb2.OptimizationResponse.SerializeToString,
-        ),
+            'OptimizePrompt': grpc.unary_unary_rpc_method_handler(
+                    servicer.OptimizePrompt,
+                    request_deserializer=gateway__pb2.OptimizationRequest.FromString,
+                    response_serializer=gateway__pb2.OptimizationResponse.SerializeToString,
+            ),
     }
     generic_handler = grpc.method_handlers_generic_handler(
-        "gateway.Optimizer", rpc_method_handlers
-    )
+            'gateway.Optimizer', rpc_method_handlers)
     server.add_generic_rpc_handlers((generic_handler,))
-    server.add_registered_method_handlers("gateway.Optimizer", rpc_method_handlers)
+    server.add_registered_method_handlers('gateway.Optimizer', rpc_method_handlers)
 
 
-# This class is part of an EXPERIMENTAL API.
+ # This class is part of an EXPERIMENTAL API.
 class Optimizer(object):
     """Missing associated documentation comment in .proto file."""
 
     @staticmethod
-    def OptimizePrompt(
-        request,
-        target,
-        options=(),
-        channel_credentials=None,
-        call_credentials=None,
-        insecure=False,
-        compression=None,
-        wait_for_ready=None,
-        timeout=None,
-        metadata=None,
-    ):
+    def OptimizePrompt(request,
+            target,
+            options=(),
+            channel_credentials=None,
+            call_credentials=None,
+            insecure=False,
+            compression=None,
+            wait_for_ready=None,
+            timeout=None,
+            metadata=None):
         return grpc.experimental.unary_unary(
             request,
             target,
-            "/gateway.Optimizer/OptimizePrompt",
+            '/gateway.Optimizer/OptimizePrompt',
             gateway__pb2.OptimizationRequest.SerializeToString,
             gateway__pb2.OptimizationResponse.FromString,
             options,
@@ -101,5 +94,4 @@ class Optimizer(object):
             wait_for_ready,
             timeout,
             metadata,
-            _registered_method=True,
-        )
+            _registered_method=True)
